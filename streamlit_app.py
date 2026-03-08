81369fc0d8d6b997eed8116f06c2b0cbf264a0f1
Claudia Valencia <78982774+ClaudiaValencia93@users.noreply.github.com>
2026-03-07 21:40:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -449,7 +449,7 @@ with col_footer1:
     st.image(os.path.join(os.path.dirname(__file__), "img", "logo_uniandes.png"), width=105)
 with col_footer2:
     st.markdown(
-        "<div style='color:gray; font-size:0.90em; padding-top:15px; margin-left:-37px;'>"
+        "<div style='color:gray; font-size:0.90em; padding-top:15px; margin-left:-39px;'>"
         "Clasificador de textos de los ODS · Curso MLNS MAIA - Uniandes · Modelo: Regresión Logística + TF-IDF · F1-Score: 85.83%"
         "</div>",
         unsafe_allow_html=True,
